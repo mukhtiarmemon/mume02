8e72c24a2172e05ce828746e9326d922b3a76120
mukhtiarmemon <mukhtiar001@yahoo.com>
2022-01-25 10:46:22 +0500

    Update GitTest.java

--- GitTest/src/com/tests/gittest/GitTest.java
+++ GitTest/src/com/tests/gittest/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     System.out.println("Git is fun 02");
     System.out.println("Git is fun 03");
     System.out.println("Git is fun 04");
+    System.out.println("Git is fun 05");
   }
 }
